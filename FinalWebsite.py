92a579f88ecd631258323c38410081bbd5f420c2
Andrew <andrew.r.hamel@gmail.com>
2018-04-22 17:30:02 -0400

    Added jquery code.

--- FinalWebsite.py
+++ FinalWebsite.py
@@ -17,7 +17,20 @@ media = form.getvalue("media")
 
 print("""
 <html>
+
+<head>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
+integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"</script>
+<script src="js/jquery.ui.autocomplete.html.js" />
+<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.min.js"</script>
+
 <title> Database Project </title>
+
+</head>
+
 <body>
 <h1>Create Growth Curve from COMETS</h1>
 <form name="myForm" action="https://bioed.bu.edu/cgi-bin/students_18/GroupB/FinalWebsite.py" method="POST">
@@ -25,6 +38,16 @@ print("""
 
 
 <div>
+<script src="bootstrap-autocomplete.min.js"></script>
+
+<form autocomplete="off" action="/action_page.php">
+  <div class="autocomplete" style="width:300px;">
+    <input id="model" type="text" name="myCountry" placeholder="Choose Models">
+  </div>
+  <input type="submit">
+</form>
+
+
     <label for="model">Model</label>
     <input id="model" size="30" value = "%s" type="text" />
 
@@ -53,19 +76,21 @@ print("""</body></html>""")
 
 
 def submit_MODEL(model,media):
+	query = ""
 	if model is not None:
 		m = model.split(',')
-		query = """SELECT MODEL.name, MEDIA.name FROM MODEL JOIN WHERE MEDIA.name = "%s" AND MODEL.name LIKE """%(media)
+		query = """SELECT MODEL.name, MEDIA.name FROM MODEL JOIN MEDIA WHERE MEDIA.name = "%s" AND MODEL.name LIKE """%(media)
 		query += "\"" + m[0] + "\""
 		if m > 1:
 			for j in m[1:]:
 				query += " AND MODEL.name LIKE \"" + j + "\""
+
 	return query
 
 
 def execute_query(query):
 	# connect to the database
-	connection = pymysql.connect(host="",db="",user="",passwd="")
+	connection = pymysql.connect(host="bioed.bu.edu",db="groupB",user="ahamel19",passwd="Sparticus6")
 
 	# get cursor
 	cursor = connection.cursor()
@@ -83,9 +108,8 @@ def execute_query(query):
 	# return the results
 	return results
 
-query = submit_MODEL(model,media)
-print(execute_query(query))
-
+#query = submit_MODEL(model,media)
+#print(execute_query(query))
 
 
 
