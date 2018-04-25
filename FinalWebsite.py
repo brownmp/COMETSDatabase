567be02540640eb21c6f6b5172998828fce1fb3f
Jon <jrodiger@users.noreply.github.com>
2018-04-25 13:20:02 -0400

    Added media dropdown and cleaned up unnecessary code/indentation issues

--- FinalWebsite.py
+++ FinalWebsite.py
@@ -6,73 +6,60 @@ import cgitb
 cgitb.enable()
 
 # print content-type
-print("Content-Type:text/html")
-
-
-#get the form
-form = cgi.FieldStorage()
-model = form.getvalue("model")
-media = form.getvalue("media")
-
+print("Content-Type:text/html\n")
 
 print("""
 <html>
 
 <head>
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
-integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"</script>
-<script src="js/jquery.ui.autocomplete.html.js" />
-<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.min.js"</script>
-
-<div class="jumbotron text-center">
-  <h1>Database Project</h1>
-  <p>Virtual experiments using COMETS!</p> 
-</div>
-
-
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 </head>
 
 <body>
-<div class="row" style="padding-left: 50px;">
-  <div class="col-sm-4">
-  	<form name="myForm" action="https://bioed.bu.edu/cgi-bin/students_18/GroupB/FinalWebsite.py" method="POST">
-		<h2>Select Model and Media</h2>
+	<div class="jumbotron text-center">
+		<h1>Database Project</h1>
+		<p>Virtual experiments using COMETS!</p> 
+	</div>
 
+	<div class="row" style="padding-left: 50px;">
+		<div class="col-sm-4">
+			<form name="myForm" action="https://bioed.bu.edu/cgi-bin/students_18/GroupB/FinalWebsite.py" method="POST">
+				<h2>Select Model and Media</h2>
 
-		<script src="bootstrap-autocomplete.min.js"></script>
+				<div class="form-group">
 
-		<form autocomplete="off" action="/action_page.php">
-		  <div class="autocomplete" style="width:300px;">
-		    <input id="model" class="form-control" type="text" name="myCountry" placeholder="Choose Models">
-		  </div>
-		  <input type="submit" class="btn">
-		</form>
+				    <label for="model">Model</label>
 
-		<div class="form-group">
-		    <label for="model">Model</label>
-		    <input id="model" class="form-control" size="30" value = "%s" type="text" />
+				    <input id="model" class="form-control" type="text" name="model" placeholder="Choose Models">
 
-		    <label for="media">Media</label>
-		    <input id="media" class="form-control" size="30" value = "%s" type="text" />
+				    <br>
 
-		   	<input type="submit" class="btn" value="Submit" >
+				    <label for="media">Media</label>
 
+				    <select class="form-control">
+				    	<option value=""</option>
+						<option value="Basal">Basal</option>
+						<option value="M9">M9</option>
+					</select>
+
+				</div>
+
+				<input type="submit" class="btn" value="Submit" >
+			</form>
+			<button type="button" class="btn">Advanced Search</button>
 		</div>
+	</div>
+</body>
+</html>
+""")
 
-		<button type="button" class="btn">Advanced Search</button>
-	</form>
-  </div>
-</div>
-</body></html>
-</table></body></html>
-
-"""%(model,media)) 
-
-print("""</body></html>""")
-
+#get the form
+form = cgi.FieldStorage()
+#model = form.getvalue("model")
+#media = form.getvalue("media")
 
 def submit_MODEL(model,media):
 	query = ""
@@ -109,23 +96,3 @@ def execute_query(query):
 
 #query = submit_MODEL(model,media)
 #print(execute_query(query))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
