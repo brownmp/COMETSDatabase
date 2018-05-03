de0d3b8f6480f300f4a8d3337b9cb5203911d15e
Jon <jrodiger@users.noreply.github.com>
2018-05-03 12:39:40 -0400

    Added tabs for basic, advanced, about, and help pages (pages unfinished)

--- Homepage.py
+++ Homepage.py
@@ -24,10 +24,29 @@ def printHead():
 	print("""
 	<html>
 	<head>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+		<script>
+			$( "#model" ).autocomplete({
+				minLength: 2,
+				source: function(request, response) {
+					$.ajax({
+						type: "POST",
+						url: "https://bioed.bu.edu/cgi-bin/students_18/GroupB/autocomplete.py",
+						data: "tag_part=" + request.term,
+						dataType: "html",
+						success: function(data) {
+							response(data);
+						}
+					});
+				},
+			});
+		</script>
 	</head>
 	""")
 
@@ -38,44 +57,94 @@ def printHomepage():
 			<h1>Biological Databases Project</h1>
 			<p>Virtual experiments using COMETS!</p>
 		</div>
-		<div class="container-fluid" style="padding-left: 50px;">
-			<form name="myForm" action="https://bioed.bu.edu/cgi-bin/students_18/GroupB/Homepage.py" method="POST">
-				<div class="row">
-					<div class="col">
-						<div class="col-8">
-							<h2>Select Model and Media</h2>
 
+		<div class="container">
+			<ul class="nav nav-tabs">
+				<li class="active"><a data-toggle="tab" href="#Basic">Basic</a></li>
+				<li><a data-toggle="tab" href="#Advanced">Advanced</a></li>
+				<li><a data-toggle="tab" href="#About">About</a></li>
+				<li><a data-toggle="tab" href="#Help">Help</a></li>
+			</ul>
+
+			<div class="tab-content">
+			    <div id="Basic" class="tab-pane fade in active">
+				    <div class="container-fluid" style="padding-left: 50px;">
+						<form name="myForm" action="https://bioed.bu.edu/cgi-bin/students_18/GroupB/Homepage.py" method="POST">
+							<div class="row">
+								<div class="col">
+									<div class="col-8">
+										<h2>Select Model and Media</h2>
+
+										<div class="form-group">
+
+										    <label for="model">Model</label>
+
+										    <input id="model" class="form-control" type="text" name="model" placeholder="Choose Models">
+
+										    <br>
+
+										    <label for="media">Media</label>
+
+										    <select class="form-control">
+										    	<option value="">Choose Media</option>
+												<option value="Basal">Basal</option>
+												<option value="M9">M9</option>
+											</select>
+
+											<br>
+
+										</div>
+
+										<input type="submit" class="btn btn-success" value="Submit" >
+									</div>
+									<div class="col-4">
+									</div>
+								</div>
+
+								<div class="col">""")
+	graph()
+	print("""
+		    </div>
+		    <div id="Advanced" class="tab-pane fade">
+			    <div class="row">
+					<h1>Advanced Search</h1>
+				</div>
+				<form name="myForm" action="https://bioed.bu.edu/cgi-bin/students_18/GroupB/Advanced.py" method="POST">
+					<div class="row">
+						<div class="col">
 							<div class="form-group">
-
 							    <label for="model">Model</label>
-
-							    <input id="model" class="form-control" type="text" width="100" name="model" placeholder="Choose Models">
-
+							    <input id="model" class="form-control" type="text" name="model" placeholder="Choose Models">
 							    <br>
-
 							    <label for="media">Media</label>
-
 							    <select class="form-control">
-							    	<option value="">Choose Media</option>
+							    	<option value=""</option>
 									<option value="Basal">Basal</option>
 									<option value="M9">M9</option>
 								</select>
-
-								<br>
-
 							</div>
-
 							<input type="submit" class="btn btn-success" value="Submit" >
-
-							<a href="https://bioed.bu.edu/cgi-bin/students_18/GroupB/Advanced.py" class="btn btn-secondary">Advanced</a>
+							<a href="https://bioed.bu.edu/cgi-bin/students_18/GroupB/Homepage.py" class="btn btn-secondary">Basic</a>
 						</div>
-						<div class="col-4">
+
+						<div class="col">
+							<div class="form-group">
+								<label for="reactions">Reactions</label>
+								<input id="reactions" class="form-control" type="text" name="reactions" placeholder="Choose Reactions">
+								<br>
+							</div>
 						</div>
-					</div>
 
-					<div class="col">
-
-					
+						<div class="col">
+							<div class="form-group">
+							<label for="metabolites">Metabolites</label>
+							<input id="metabolites" class="form-control" type="text" name="metabolites" placeholder="Choose Metabolites">
+							<br>
+						</div>
+					</div>	
+				</form>
+		    </div>
+		</div>			
 	""")
 
 
@@ -149,4 +218,3 @@ def graph():
 
 printHead()
 printHomepage()
-graph()
